d5c773e2fc7fbee637b16c9e740f4fc1c09bbbd7
myogic <moh.yogi10065@gmail.com>
2022-02-08 10:41:56 +0700

    fixing delete

--- routers/Master-Items.js
+++ routers/Master-Items.js
@@ -9,7 +9,7 @@ router.get('/item/itemnull', jwt.verify, jwt.cekrole(["1", "2", "3"]), items.dat
 router.get('/item', jwt.verify, jwt.cekrole(["1", "2", "3"]), items.itemById);
 router.post('/item', jwt.verify, jwt.cekrole(["1", "2", "3"]), items.additem);
 router.put('/item/:item_id', jwt.verify, jwt.cekrole(["1", "2", "3"]), items.edititem);
-router.delete('.item/:id', jwt.verify, jwt.cekrole(["1", "2", "3"]), items.hapusitem);
+router.delete('/item/:id', jwt.verify, jwt.cekrole(["1", "2", "3"]), items.hapusitem);
 router.get('/item/:id', jwt.verify, jwt.cekrole(["1", "2", "3"]), items.itemByitemId);
 router.get('/item/search/:tag', jwt.verify, items.search);
 
