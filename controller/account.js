2cb701d4270a47f6eb99d1036ec7c30e13d05343
yogi <moh.yogi10065@gmail.com>
2022-04-22 14:15:58 +0700

    add try cacth

--- controller/account.js
+++ controller/account.js
@@ -3,19 +3,22 @@ var response = require('../res');
 var koneksi = require('../koneksi');
 // get all
 exports.allAccount = function(req, res) {
-
-    koneksi.query('SELECT * FROM account', function(error, rows, fields) {
-        if (error) {
-            console.log(error);
-        } else {
-            return res.send({
-                status: 'success',
-                message: req.t("account.success_get_account"),
-                data: rows
-            })
-        }
-    });
-
+    try{
+        koneksi.query('SELECT * FROM account', function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                return res.send({
+                    status: 'success',
+                    message: req.t("account.success_get_account"),
+                    data: rows
+                })
+            }
+        });    
+    }catch(err){
+        const {status, data} = err.message;
+        return res.status(status).json({status,data});
+    }
 };
 
 
@@ -42,30 +45,39 @@ exports.editAccount = function(req, res) {
         }
     });
     }catch(error){
+        const {status, data} = error.message;
+        return res.status(status).json({status,data});
 
     }
 };
 
 //get account by id
 exports.accountById = function(req, res) {
-    let id = req.params.id;
-    koneksi.query('SELECT * FROM account WHERE id_account= ?', [id], function(error, rows, fields) {
-        if (error) {
-            console.log(error);
-        } else {
-            return res.send({
-                status: 'success',
-                message: req.t('account.success_get_account'),
-                data: rows
-            })
-        }
-
-    });
+    try{
+        let id = req.params.id;
+        koneksi.query('SELECT * FROM account WHERE id_account= ?', [id], function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                return res.send({
+                    status: 'success',
+                    message: req.t('account.success_get_account'),
+                    data: rows
+                })
+            }
+    
+        });
+    }catch(err){
+        const {status, data} = err.message;
+        return res.status(status).json({status,data});
+    }
+ 
 };
 
 // add account
 exports.addaccount = function(req, res) {
-    const id_account = req.body.id_account,
+    try{
+        const id_account = req.body.id_account,
         name = req.body.name,
         description = req.body.description,
         email = req.body.email,
@@ -91,21 +103,31 @@ exports.addaccount = function(req, res) {
                     })
                 }
             });
+    }catch(err){
+        const {status, data} = err.message;
+        return res.status(status).json({status,data});
+    }
+
 };
 
 
 // delete account
 exports.hapusaccount = function(req, res) {
-    var id_account = req.body.id_account;
-    koneksi.query('DELETE FROM account WHERE id_account=?', [id_account],
-        function(error, rows, fields) {
-            if (error) {
-                console.log(error);
-            } else {
-                res.send({
-                    status: 'success',
-                    message: req.t('account.succes_delete_account')
-                })
-            }
-        });
+    try{
+        const id_account = req.body.id_account;
+        koneksi.query('DELETE FROM account WHERE id_account=?', [id_account],
+            function(error, rows, fields) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    res.send({
+                        status: 'success',
+                        message: req.t('account.succes_delete_account')
+                    })
+                }
+            });
+    }catch(err){
+        const {status, data} = err.message;
+        return res.status(status).json({status,data});
+    }
 }
