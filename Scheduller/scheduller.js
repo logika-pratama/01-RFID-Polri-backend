722c534a44b10c607fe8615ba32f477b36712b70
yogi <moh.yogi10065@gmail.com>
2022-05-24 18:25:26 +0700

    cahnge to 1 s

--- Scheduller/scheduller.js
+++ Scheduller/scheduller.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const PostData = require('../controller/ITAM/handler/GateOutCek');
 
 cron.schedule('* * * * * *', function() {
-  console.log('Running task every 60 second');
+  console.log('Running task every 1 second');
   console.log(new Date().toLocaleString())
   PostData.gate();
 });
